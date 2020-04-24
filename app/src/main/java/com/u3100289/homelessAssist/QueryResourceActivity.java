d3bf822b1237a746e764664fb16e3c4d5be23f93
Mitchell Hunt-Sharman <mitchell.huntsharman@hotmail.com>
2020-04-24 11:52:20 +1000

    query device activity

---
+++ app/src/main/java/com/u3100289/homelessAssist/QueryResourceActivity.java
@@ -0,0 +1,16 @@
+package com.u3100289.homelessAssist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.u3100289.homelessAssist.R;
+
+public class QueryResourceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_query_resource);
+    }
+}

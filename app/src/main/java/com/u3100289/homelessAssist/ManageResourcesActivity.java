9630e3dfbd63976a6f753ab2e501a7f3eb5f02f5
Mitchell Hunt-Sharman <mitchell.huntsharman@hotmail.com>
2020-04-27 01:11:05 +1000

    New Activities

---
+++ app/src/main/java/com/u3100289/homelessAssist/ManageResourcesActivity.java
@@ -0,0 +1,14 @@
+package com.u3100289.homelessAssist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageResourcesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_resources);
+    }
+}
---
+++ app/src/main/java/com/u3100289/homelessAssist/ViewUserResourcesActivity.java
@@ -0,0 +1,14 @@
+package com.u3100289.homelessAssist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewUserResourcesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_user_resources);
+    }
+}
